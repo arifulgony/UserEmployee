0018746d617df272a73f3ee7f233827f6f61d445
arifulgony <arifulgony@gmail.com>
2023-06-15 00:13:26 +0600

    project done

--- src/Components/Pages/index.jsx
+++
@@ -1,4 +0,0 @@
-// export {default as EditUserModal } from './EditUserModal';
-// export {default as EmployeeList } from './EmployeeList';
-// export {default as UserDetails} from './UserDetails';
-// export {default as UserList } from './UserList';
